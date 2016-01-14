f649d8ccfc2076e4c9919ab7ba421286a2710f6f
Daniel Magro <dmagro04@gmail.com>
2016-01-14 20:44:22 +0000

    Added silly mode

--- scripts/sketch-script.js
+++ scripts/sketch-script.js
@@ -1,26 +1,57 @@
-var gridWidth = 16;
-var gridHeight = 16;
-var cellWidth = 15;
-var cellHeight = 15;
+var MAXWIDTH = 120;
+var MAXHEIGHT = 60;
+
+var gridWidth = 60;
+var gridHeight = 30;
+var cellWidth = 10;
+var cellHeight = 10;
+
+var isSilly = false;
+//var wantGrid = true;
+
+var colors=["#009f6a","#4285F4","#ea4335","#fbbc05"];
 
 $(document).ready(function() {
 
-	addClearButton();
+	addMenu();
 	setupGrid();
 
-	$(".cell").hover(function() {
-		$(this).css("background-color", "red");
+	$("#reset-button").click(function() {
+		var isValid = getNewDimensions();
+		if(isValid){
+			$("#container").remove();
+			getNewCellDimensions();
+			isSilly = false;
+			wantGrid = true;
+			setupGrid();
+		}
+		else{
+			alert("Grid maximum size is 120x60");
+		}
 	});
 
-	$("#clear-button").click(function() {
-		$("#container").remove();
-		
+	$("#silly-button").click(function() {
+		isSilly = !isSilly;
 	});
-	
+
+	/*$("#grid-button").click(function() {
+		wantGrid = !wantGrid;
+		var boderValue = "";
+		if(wantGrid){
+			boderValue = "1px solid black";
+		}
+		else{
+			boderValue = "none";
+		}
+		$(".cell").css("border", boderValue);
+	});*/
 });
 
-function addClearButton() {
-	$("body").append("<div id='clear-button'><a href=''>Clear</a></div>");
+function addMenu() {
+	$("body").append("<div id='menu'></div>");
+	//$("#menu").append("<div class='button' id='grid-button'>Grid</div>")
+	$("#menu").append("<div class='button' id='reset-button'>Reset</div>");
+	$("#menu").append("<div class='button' id='silly-button'>Silly</div>");
 }
 
 function setupGrid() {
@@ -33,9 +64,43 @@ function setupGrid() {
 }
 
 function setupCells() {
+
 	for (var i = 0; i < gridWidth; i++) {
 		for (var j = 0; j < gridHeight; j++) {
 			$("<div class='cell'></div>").height(cellHeight).width(cellWidth).appendTo("#container");
 		}
 	}
+
+	$(".cell").hover(function() {
+		if(isSilly) {
+			$(this).css("background-color", colors[Math.floor(Math.random() * colors.length)]);
+		}
+		else {
+			$(this).css("background-color", "black");
+		}
+	});
+}
+
+function getNewDimensions() {
+	var inputDimensions = prompt("Please enter the new grid dimensions as the example: width,height", "16,16");
+
+	inputDimensions = inputDimensions.replace(/\s/g,'');
+	var splittedDimensions = inputDimensions.split(",");
+	if(splittedDimensions !== null ) {
+		if (splittedDimensions.length !== 2) {
+			alert("Dimensions need to be separated as these examples(white spaces are ignored): \n width,height \n width , height");
+		}
+		else {
+			var newWidth = parseInt(splittedDimensions[0]);
+			var newHeight = parseInt(splittedDimensions[1]);
+
+			if(newWidth <= MAXWIDTH && newHeight <= MAXHEIGHT){
+				gridWidth = newWidth;
+				gridHeight = newHeight;
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
